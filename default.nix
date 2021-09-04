718aaaf0204d0662f84dc9130c559c4906578a2b
Ed Cavazos <wayo.cavazos@gmail.com>
2021-09-04 02:46:00 -0700

    Testing new ihp commit

--- default.nix
+++ default.nix
@@ -1,8 +1,16 @@
 let
+
+    # ihp = builtins.fetchGit {
+    #     url = "https://github.com/digitallyinduced/ihp.git";
+    #     ref = "refs/tags/v0.12.0";
+    # };
+
     ihp = builtins.fetchGit {
         url = "https://github.com/digitallyinduced/ihp.git";
-        ref = "refs/tags/v0.12.0";
+        rev = "ea6bcf73c93dd5a1f3440e39c877ec7da16dc43b";
+        ref = "*";
     };
+
     haskellEnv = import "${ihp}/NixSupport/default.nix" {
         ihp = ihp;
         haskellDeps = p: with p; [
